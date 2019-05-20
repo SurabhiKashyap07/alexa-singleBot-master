95885a8d79108bc1316ce6d8d26554c93c73daa6
surabhikashyap07 <surabhi.kashyap07@gmail.com>
2019-05-21 00:55:57 +0530

    Testing

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ module.exports = function () {
     waitForMoreResponsesMs: 200,  //milliseconds to wait for additional webhook responses
             
     amzn_appId: "amzn1.ask.skill.df22d270-6bc3-4a37-8e53-b8d7a1f1717f",
-    channelSecretKey: 'OQt8CkMM9EuatvjKR8IHamrzMgMjzmil', // exxaro
+    channelSecretKey: 'OQt8CkMM9EuatvjKR8IHamrzMgMjzmil', // edmj
     //ninja botchannelSecretKey: 'AIWvBPHXhxhFdKV47IDu1fF2pxcsvmLn',
     channelUrl: 'https://b2b34218.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/F2B064FD-6D0E-43B0-A0BF-22766F4C2775'        
     //ninja bot channelUrl: 'https://b2b34218.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/E78A0816-F388-4C3E-AF93-1D1C543E931A'        
