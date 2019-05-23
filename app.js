436ca520b0423f8c5292dfecdaee97d6d8496138
SurabhiKashyap07 <50844906+SurabhiKashyap07@users.noreply.github.com>
2019-05-23 14:40:40 +0530

    alexa_test_bot update
    alexa_test_bot update

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ module.exports = function () {
     waitForMoreResponsesMs: 200,  //milliseconds to wait for additional webhook responses
             
     amzn_appId: "amzn1.ask.skill.be571df0-a2a1-484f-8d41-39ba996436dd",
-    channelSecretKey: 'fOTdjIGnnyyfaPcK4cPjpxws5R6KAfzU', // edmj
+    channelSecretKey: 'NGbUUzh5LkmmRLAClpAriF75FpDDMluI', // edmj
     //ninja botchannelSecretKey: 'AIWvBPHXhxhFdKV47IDu1fF2pxcsvmLn',
-    channelUrl: 'https://b2b34218.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/F2B064FD-6D0E-43B0-A0BF-22766F4C2775'        
+    channelUrl: 'https://b2b34218.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/D3502546-7177-41B4-A7A9-BAC8A3B9911F'        
     //ninja bot channelUrl: 'https://b2b34218.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/E78A0816-F388-4C3E-AF93-1D1C543E931A'        
     // channelUrl: 'https://b2b34218.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/E78A0816-F388-4C3E-AF93-1D1C543E931A'    
     // channelUrl: 'https://e87476aa.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/AE1803AB-3A46-4A40-A34A-13B4989C5DD5'
